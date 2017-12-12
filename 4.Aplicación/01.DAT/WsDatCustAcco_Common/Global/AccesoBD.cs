df7f614ab084661fdeec6d38093b5203aae84dd3
Alex Gamboa <alex_gam.s@hotmail.com>
2017-12-11 19:00:23 -0600

    w

--- 4.Aplicación/01.DAT/WsDatCustAcco_Common/Global/AccesoBD.cs
+++ 4.Aplicación/01.DAT/WsDatCustAcco_Common/Global/AccesoBD.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,31 @@ namespace WsDatCustAcco_Common.Global
 {
     public class AccesoBD
     {
+        #region [Variables]
+
+        SqlConnection conexion;
+        
+        #endregion [Variables]
         // Se debe crear la clase con la conexión a la base de datos. GianCarlos nos va a ayudar con esto.
 
         public String ProcedimientoMantenimiento { get; set; }
+
+        public bool Conexion()
+        {
+
+            try
+            {
+                SqlConnection conexion = new SqlConnection("server=msqlaws.csiftz7i1ill.us-east-1.rds.amazonaws.com,1433; database=BDCustAcco ; integrated security = true");
+                conexion.Open();
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+            //conexion.Close();
+
+            return true;
+        }
     }
 }
